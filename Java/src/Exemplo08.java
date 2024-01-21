06eb143fad2072fe6057e63be46b96dd6b934afb
Victor Dantas <dev.victordantas@gmail.com>
2024-01-21 19:54:08 -0300

    Learning Do While

--- Java/src/Exemplo08.java
+++ Java/src/Exemplo08.java
@@ -1,3 +1,11 @@
 public class Exemplo08 {
-  
+  public static void main(String[] args) {
+
+    int numero = 1;
+
+    while (numero <= 5) {
+      System.out.println(numero);
+      numero++;
+    }
+  }
 }
